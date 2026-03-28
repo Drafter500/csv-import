2ef57aef9bfa9f4aa53b5c7f60f8ed9fb64101a3
drafter500 <vzemtsov@yahoo.com>
2026-03-28 18:23:21 +0100

    Updates style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 
 function App() {
   return (
-    <Box sx={{ p: 3, maxWidth: '1200px', margin: '0 auto' }}>
+    <Box sx={{ p: 3, maxWidth: '1200px', margin: '0 auto', display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <CsvImporter />
     </Box>
   )
